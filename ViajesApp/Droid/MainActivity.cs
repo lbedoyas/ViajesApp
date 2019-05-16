71ded400f88a6f5515c1fb13fa1401f40d51a894
lbedoyas <lucho.bedoya1@hotmail.com>
2019-05-15 20:10:05 -0500

    Adicional elemntos Main
    Se realiza findById de los elementos que se requieren creados en el XML y se crea un evento click para condicionar el login

--- ViajesApp/Droid/MainActivity.cs
+++ ViajesApp/Droid/MainActivity.cs
@@ -1,13 +1,15 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace ViajesApp.Droid
 {
     [Activity(Label = "ViajesApp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        EditText nombreusuarioEditText, passwordEditText;
+        Button inicioSessionButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,11 +18,23 @@ namespace ViajesApp.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            nombreusuarioEditText = FindViewById<EditText>(Resource.Id.usuarioEditText);
+            passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+            inicioSessionButton = FindViewById<Button>(Resource.Id.inicioSesionButton);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            inicioSessionButton.Click += InicioSessionButton_Clicked;
+        }
+
+        private void InicioSessionButton_Clicked (object sender, EventArgs argumentos)
+        {
+            if(string.IsNullOrEmpty(nombreusuarioEditText.Text) || string.IsNullOrEmpty(passwordEditText.Text))
+            {
+
+            }
+            else
+            {
+                //hacer: navegar a la pagina principal
+            }
         }
     }
 }
